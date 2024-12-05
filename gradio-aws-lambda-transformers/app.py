cf56637545813a78c92d6744be14c7d902c084f3
lukmee4826 <lukmeepook@gmail.com>
2024-12-05 12:59:02 +0700

    fix it to this era

--- gradio-aws-lambda-transformers/app.py
+++ gradio-aws-lambda-transformers/app.py
@@ -1,31 +1,30 @@
 import gradio as gr
 from transformers import pipeline
 
+# Load transformers pipeline from a local directory (model)
 clf = pipeline("sentiment-analysis", model="model/")
 
-
+# Predict function used by Gradio
 def sentiment(payload):
     prediction = clf(payload, return_all_scores=True)
-
-    # convert list to dict
+    # Convert list to dict
     result = {}
     for pred in prediction[0]:
         result[pred["label"]] = pred["score"]
     return result
 
+# Build the Gradio app
+with gr.Blocks() as gradio_app:
+    gr.Markdown("### Sentiment Analysis")
+    text_input = gr.Textbox(placeholder="Enter a positive or negative sentence here...")
+    output_label = gr.Label()
+    examples = gr.Examples(
+        examples=["I Love Serverless Machine Learning", "Running Gradio on AWS Lambda is amazing"],
+        inputs=text_input
+    )
+    classify_button = gr.Button("Analyze Sentiment")
+    
+    classify_button.click(fn=sentiment, inputs=text_input, outputs=output_label)
 
-demo = gr.Interface(
-    fn=sentiment,
-    inputs=gr.Textbox(placeholder="Enter a positive or negative sentence here..."),
-    outputs="label",
-    interpretation="default",
-    examples=[["I Love Serverless Machine Learning"],["Running Gradio on AWS Lambda is amazing"]],
-    allow_flagging="never",
-    analytics_enabled=False
-)
-
-demo.launch(
-    server_port=8080,
-    # server_name="0.0.0.0",
-    enable_queue=False,
-)
+# Run the app
+gradio_app.launch(share=True)
